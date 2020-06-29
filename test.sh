cd4df312607462bb07a57a40aaf10cd622913491
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2020-06-29 16:09:48 -0400

    Making test.sh for tox and test_local.sh for local pipenv.

--- test.sh
+++ test.sh
@@ -5,7 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd ${DIR}
 
-# Remove and reinstall pipenv to validate Pipenv
-pipenv --rm
-pipenv sync
-pipenv run pytest tests
+tox
