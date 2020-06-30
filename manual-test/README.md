be05038841d0b43515d1ea5fa349dd5686a67a0d
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2020-06-29 20:41:07 -0400

    Making full testing script for manual testing.

--- manual-test/README.md
+++ manual-test/README.md
@@ -3,7 +3,10 @@
 This directory holds tests of the client into the network
 with `CasperLabs/hack/docker`.
 
- - Run `./build_contracts.sh` to have any wasm used built.
+ - Run `./build_casperlabs.sh` to have any wasm used built and build docker iamges.
  - Run `./standup.sh` to bring up 3 node network.
- - Run python and cli tests.
+ - Run python tests.
  - Run `./teardown.sh` to bring down network when finished.
+
+This assumes that `client-py` repo is at the same directory level as the 
+`CasperLabs` repo.  
--- manual-test/standup.sh
+++ manual-test/standup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+docker pull casperlabs/execution-engine:dev
+docker pull casperlabs/node:dev
+docker pull casperlabs/key-generator:dev
+
+export CL_VERSION=dev
+
 # Stand up highway network
 cd ../../CasperLabs/hack/docker || exit
 make .casperlabs
@@ -7,3 +13,7 @@ make node-0/up
 make node-1/up
 make node-2/up
 
+# Sleep until network starts.
+while ! docker logs node-0 | grep "Executing action=StartRound(" ; do echo "Waiting for network startup.  Sleeping..."; sleep 10; done
+
+echo "Network ready."
