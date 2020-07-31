ac0d85d4b2667f4cf9742853ba9300af84812e2e
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2020-07-30 22:59:46 -0400

    Initial hack/docker. Adding contracts needed for new additions to test_slow.py.

--- manual-test/build_contracts.sh
+++ manual-test/build_contracts.sh
@@ -3,5 +3,8 @@
 # build contracts to use
 pushd ../../CasperLabs/execution-engine || exit
 cargo build --package faucet --target wasm32-unknown-unknown --release
+cargo build --package test-payment-stored --target wasm32-unknown-unknown --release
+cargo build --package transfer-to-account-u512-stored --target wasm32-unknown-unknown --release
 popd
+
 
