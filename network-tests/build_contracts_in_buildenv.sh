d7d1d988d802dc01372d0e0fc6c63d9ab778fec4
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2020-08-06 13:29:24 -0400

    Using buildenv CI is running under instead of our own.

--- network-tests/build_contracts_in_buildenv.sh
+++ network-tests/build_contracts_in_buildenv.sh
@@ -10,9 +10,21 @@ if [[ -z "$DRONE_BUILD_NUMBER" ]]; then
          casperlabs/buildenv:latest \
          /bin/bash -c "$(cat $DIR/internal_build_script)"
 else
-  # inside CI, so we are running in docker and need extra volume for docker.sock
-  docker run --rm -v $DIR/wasm:/wasm -v /tmp:/tmp \
-         -v /var/run/docker.sock:/var/run/docker.sock \
-         casperlabs/buildenv:latest \
-         /bin/bash -c "$(cat $DIR/internal_build_script)"
+  # inside CI, so we are running in buildenv docker
+  cd /tmp || exit 1
+  git clone --depth 1 https://github.com/CasperLabs/CasperLabs
+
+  cd /tmp/CasperLabs/execution-engine || exit 1
+  cargo build --package faucet --target wasm32-unknown-unknown --release
+  cargo build --package do-nothing --target wasm32-unknown-unknown --release
+  cargo build --package test-payment-stored --target wasm32-unknown-unknown --release
+  cargo build --package transfer-to-account-u512-stored --target wasm32-unknown-unknown --release
+
+  cp target/wasm32-unknown-unknown/release/*.wasm "$DIR/wasm"
+  cd /
+  rm -rf /tmp/CasperLabs
+#  docker run --rm -v $DIR/wasm:/wasm -v /tmp:/tmp \
+#         -v /var/run/docker.sock:/var/run/docker.sock \
+#         casperlabs/buildenv:latest \
+#         /bin/bash -c "$(cat $DIR/internal_build_script)"
 fi
